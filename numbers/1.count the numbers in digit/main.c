0b48111bdbb5e8ea7c80ab4c515f6425f6b173c0
Solaman10 <89968516+Solaman10@users.noreply.github.com>
2021-09-03 17:52:57 +0530

    Update main.c

--- numbers/1.count the numbers in digit/main.c
+++ numbers/1.count the numbers in digit/main.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,count=0;
+    printf("Enter the digits:");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        count++;
+        n=n/10;
+    }
+    printf("Total numbers in digits: %d",count);
+    return 0;
+}
