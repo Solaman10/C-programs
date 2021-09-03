7a8a478251181193c071356d63877142beb3f590
Solaman10 <89968516+Solaman10@users.noreply.github.com>
2021-09-03 18:11:40 +0530

    Update main.c

--- numbers/2.sum of digit of a number/main.c
+++ numbers/2.sum of digit of a number/main.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0;
+    printf("Enter the digits:");
+    scanf("%d",&n);
+    
+    while(n!=0)
+    {
+        r=n%10;
+        sum+=r;
+        n=n/10;
+    }
+    printf("\nSum of digits:%d",sum);
+    return 0;
+}
