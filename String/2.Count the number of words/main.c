f321a74a542172aa7411df265acafa784e5643c6
Solaimani <89968516+Solaman10@users.noreply.github.com>
2021-12-19 19:02:31 +0530

    Update main.c

--- String/2.Count the number of words/main.c
+++ String/2.Count the number of words/main.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+int main()
+{
+    char str[]="anything is nothing,even it has something";
+    int strlen=sizeof(str)/sizeof(char)-1;
+    int count=1;
+    
+    for(int i=0;i<strlen;i++)
+    {
+        while(str[i+1]==' ' || str[i+1]==',')
+        {
+            i++;
+        }
+        if(str[i]==' ' || str[i]==',')
+        {
+            count++;
+        }
+    }
+    printf("%d words are in this sentence",count);
+    
+    return 0;
+
+}
