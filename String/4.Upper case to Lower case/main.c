4ad7e3d77adc1648d240689948fda898a62535f6
Solaimani <89968516+Solaman10@users.noreply.github.com>
2021-12-19 19:20:20 +0530

    Update main.c

--- String/4.Upper case to Lower case/main.c
+++ String/4.Upper case to Lower case/main.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+int lower_case_word(char letter)
+{
+    return letter>='A' && letter<='z';
+}
+
+
+int main()
+{
+    char name[]="MANI";
+    int len=sizeof(name)/sizeof(char)-1;
+    
+    for(int i=0;i<len;i++)
+    {
+      if(lower_case_word(name[i]))
+       {
+          name[i]+=32;
+       }
+    }
+    printf("%s",name);
+    
+    return 0;
+}
