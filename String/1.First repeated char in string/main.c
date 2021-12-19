33c36dc19ec099438f46feeefc64c53e0c72d5e4
Solaimani <89968516+Solaman10@users.noreply.github.com>
2021-12-19 18:44:23 +0530

    Update main.c

--- String/1.First repeated char in string/main.c
+++ String/1.First repeated char in string/main.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
 
+int main()
+{
+    char word[]="Elon Musk";
+    int length = sizeof(word)/sizeof(char)-1,i,j;
+    char firstRepeatedLetter;
+    
+    for(i=0;i<length;i++)
+    {
+        for(j=i+1;j<length;j++)
+        {
+            if(word[i]==word[j])
+            {
+                firstRepeatedLetter=word[i];
+                break;
+            }
+        }
+        if(firstRepeatedLetter)
+        break;
+    }
+    if(firstRepeatedLetter)
+      printf("%c",firstRepeatedLetter);
+    else
+      printf("There is no repeating character in this sentence");
+      
+    return 0;
+    
+}
