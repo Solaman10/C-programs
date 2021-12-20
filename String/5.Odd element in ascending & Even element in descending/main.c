31d1007d76147c824e19b661901844f277f245cd
Solaimani <89968516+Solaman10@users.noreply.github.com>
2021-12-20 19:42:05 +0530

    Update main.c

--- String/5.Odd element in ascending & Even element in descending/main.c
+++ String/5.Odd element in ascending & Even element in descending/main.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+int main()
+{
+    char str[] = "SOLAIMANI";
+    int size = sizeof(str)/sizeof(char) - 1;
+    int i = 0, count = 0;
+    int j = (size%2 == 0) ? size - 1 : size - 2;
+    
+    while(count<size)
+    {
+        if(i<=size)
+        {
+          printf("%c",str[i]);
+          i+=2;
+        }  
+        else
+        {
+          printf("%c",str[j]);
+          j-=2;
+        }
+        count++;
+        
+    }
+    printf("\n");
+    return 0;
+}
