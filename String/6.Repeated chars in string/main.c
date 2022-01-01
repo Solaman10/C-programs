d2ae0cde500d15cfc0d33e613bba1323ecf9dbef
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-01-01 19:14:16 +0530

    Update main.c

--- String/6.Repeated chars in string/main.c
+++ String/6.Repeated chars in string/main.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+int main()
+{
+    char name[] = "solaimani";
+    int length = sizeof(name)/sizeof(name[0]) -1;
+    int i,j;
+    
+    for(i=0;i<length;i++)
+    {
+        int count = 1;
+        for(j=i+1;j<length;j++)
+         {
+             if(name[i] == name[j] && name[i] != '0')
+             {
+                count++;
+                name[j]='0'; //change the repeated character as 0 
+             }
+         }
+    if(count>0 && name[i] != '0')
+    {
+    printf(" %c : %d\n",name[i],count);
+    }
+    }
+    return 0;
+}
