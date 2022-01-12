907f57e22188d63bb1eb006fd6204e240a8d6ac4
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-01-12 13:30:57 +0530

    Update main.c

--- Patterns/3. X pattern/main.c
+++ Patterns/3. X pattern/main.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char word[50];
+    int len,i,j,middle;
+    scanf("%s",word);
+    
+    len=strlen(word)+1;
+    middle=len/2;
+    
+    for(i=0;i<len;i++)
+    {
+        for(j=0;j<len;j++)
+        {
+            if(i==j && middle!=i && middle!=j)
+            {
+                printf("%c ",word[j-1]);
+            }
+            else if(i+j==len)
+            {
+                printf("%c ",word[j-1]);
+            }
+            else 
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
