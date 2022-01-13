7e65a2a6ba95ed467868399594c123484e9bcd14
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-01-13 23:13:18 +0530

    Update main.c

--- Numbers/3.Sum of First and Last Digit of Number/main.c
+++ Numbers/3.Sum of First and Last Digit of Number/main.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+    int arr[50],n,i,len,ans;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    for(len=0;arr[len];len++)
+    ans=arr[0]+arr[len];
+    printf("%d",ans);
+    
+}
