2dc549d522e0145e85ea801101a828815db23bce
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-04-02 11:28:39 +0530

    Update main.c

--- Arrays/4.Sorting array/main.c
+++ Arrays/4.Sorting array/main.c
@@ -1,1 +1,39 @@
+/*
+i/p: 
+5
+4 2 5 3 1
 
+o/p: 
+1 2 3 4 5
+*/
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
