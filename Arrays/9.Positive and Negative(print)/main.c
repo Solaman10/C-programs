66b192ba8c163e024cb75906f14ef226190dce73
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-04-02 11:59:52 +0530

    Update main.c

--- Arrays/9.Positive and Negative(print)/main.c
+++ Arrays/9.Positive and Negative(print)/main.c
@@ -1,1 +1,40 @@
+/*
+i/p:
+6
+1 2 -3 4 6 -1
 
+o/p:
+Positive nums: 1 2 4 6
+Negative nums: -3 -1
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a[30],n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    
+    printf("\nPositive nums: ");
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>0)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+    
+    printf("\nNegative nums: ");
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<0)
+        {
+            printf("%d ",a[i]);
+        }
+    }
+    
+    return 0;
+}
