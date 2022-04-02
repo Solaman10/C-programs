3c6034349eda758b9a98ab1596c6e25d23e205cc
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-04-02 11:42:00 +0530

    Update main.c

--- Arrays/6.Reverse first half/main.c
+++ Arrays/6.Reverse first half/main.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,j,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    
+    for(i=0,j=n/2-1;i<j;i++,j--)
+    {
+        temp = a[i];
+        a[i]=a[j];
+        a[j]=temp;
+    }
+  
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
