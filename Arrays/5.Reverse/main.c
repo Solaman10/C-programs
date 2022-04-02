256a00c0a25f77130e34b0808bd761933076c714
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-04-02 11:33:24 +0530

    Update main.c

--- Arrays/5.Reverse/main.c
+++ Arrays/5.Reverse/main.c
@@ -1,1 +1,26 @@
+/*
+i/p:
+5
+1 2 3 4 5
 
+o/p:
+5 4 3 2 1
+*/
+
+
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+  
+    for(i=n-1;i>=0;i--)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
