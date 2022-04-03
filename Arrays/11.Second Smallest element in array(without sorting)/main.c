fc0ccdd3f47df4ffacad35deea1f465eae7760d0
Solaimani <89968516+Solaman10@users.noreply.github.com>
2022-04-03 12:21:05 +0530

    Update and rename Arrays/11.Second Smallest element in array/main.c to Arrays/11.Second Smallest element in array(without sorting)/main.c

---
+++ Arrays/11.Second Smallest element in array(without sorting)/main.c
@@ -0,0 +1,37 @@
+/*
+i/p:
+9 
+9 1 3 4 5 7 8 6 2 
+
+o/p:
+8
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a[20],n,i,max,scndMax;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    max=a[0];
+    scndMax=0;
+    
+    for(i=0;i<n;i++)
+    {
+            if(max<a[i])
+            {
+                scndMax=max;
+                max=a[i];
+            }
+            if(scndMax<a[i] && max>a[i])
+            {
+                scndMax=a[i];
+            }
+    }
+    printf("%d",scndMax);
+    return 0;
+}
